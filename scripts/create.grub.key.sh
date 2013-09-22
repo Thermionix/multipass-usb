3f971d3f54b7b9907c399967ad25d2c934d1109a
Thermionix <bond711@gmail.com>
2013-09-22 16:02:14 +1000

    new file:   .gitignore 	deleted:    boot/grub/.gitignore 	deleted:    boot/grub/grub.cfg 	modified:   scripts/create.grub.key.sh

--- scripts/create.grub.key.sh
+++ scripts/create.grub.key.sh
@@ -11,9 +11,9 @@ read -e -p "Set disk to install to: " -i "sd" DSK
 
 blockdevice=/dev/${DSK}
 
-labelboot="multipass01"
-partboot="/dev/disk/by-partlabel/$labelboot"
-tmpdir=/tmp/$labelboot
+drivelabel="multipass01"
+partboot="/dev/disk/by-partlabel/$drivelabel"
+tmpdir=/tmp/$drivelabel
 
 echo "## WILL COMPLETELY WIPE $blockdevice"
 read -p "Press [Enter] key to continue"
@@ -22,32 +22,28 @@ echo "## creating partition bios_grub"
 sudo parted -s ${blockdevice} mklabel gpt
 sudo parted -s ${blockdevice} -a optimal unit MB mkpart primary 1 2
 sudo parted -s ${blockdevice} set 1 bios_grub on
-echo "## creating partition $labelboot"
+echo "## creating partition $drivelabel"
 sudo parted -s ${blockdevice} -a optimal unit MB -- mkpart primary 2 -1
-sudo parted -s ${blockdevice} name 2 $labelboot
+sudo parted -s ${blockdevice} name 2 $drivelabel
 
 sleep 1
-sudo mkfs.ext4 $partboot
+sudo mkfs.ext4 -L "${drivelabel}" $partboot
 
 sudo mkdir -p $tmpdir
 
 sudo mount $partboot $tmpdir
 
-sudo chown -R `whoami` $tmpdir
-
-pushd $tmpdir
-	git init .
-	git remote add -t \* -f origin https://github.com/Thermionix/multipass-usb.git
-	git fetch
-	git checkout master
-popd
-
 sudo grub-install --no-floppy --root-directory=$tmpdir ${blockdevice}
 
 sleep 1
 
+sudo chown -R `whoami` $tmpdir
+
 cp /usr/lib/syslinux/memdisk $tmpdir/boot/grub/
 
+pushd $tmpdir
+	git clone https://github.com/Thermionix/multipass-usb.git $drivelabel; shopt -s dotglob nullglob; mv $drivelabel/* .; rmdir $drivelabel
+popd
 
 echo "## will unmount $partboot when ready"
 read -p "Press [Enter] key to continue"
