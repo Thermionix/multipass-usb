a639c38925cf9cb7ecfd62618c6bef9e8a17e030
Thermionix <bond711@gmail.com>
2014-07-29 15:39:56 +1000

    modified:   resources/scripts/create.grub.key.sh

--- resources/scripts/create.grub.key.sh
+++ resources/scripts/create.grub.key.sh
@@ -8,6 +8,7 @@ command -v syslinux > /dev/null || { echo "## please install syslinux" ; exit 1 
 command -v grub-install > /dev/null || { echo "## please install grub" ; exit 1 ; }
 command -v tar > /dev/null || { echo "## please install tar" ; exit 1 ; }
 command -v curl > /dev/null || { echo "## please install curl" ; exit 1 ; }
+command -v git > /dev/null || { echo "## please install git" ; exit 1 ; }
 command -v whiptail >/dev/null 2>&1 || { echo "whiptail (pkg libnewt) required for this script" >&2 ; exit 1 ; }
 command -v sgdisk >/dev/null 2>&1 || { echo "sgdisk (pkg gptfdisk) required for this script" >&2 ; exit 1 ; }
 
@@ -22,7 +23,7 @@ if whiptail --defaultno --yesno "COMPLETELY WIPE ${DSK}?" 8 40 ; then
 	sleep 1
 
 	echo "# wiping ${DSK}"
-	sudo dd if=/dev/zero of=${DSK} bs=512 count=1
+	sudo dd if=/dev/zero of=${DSK} bs=512 count=20
 	sudo sgdisk --zap-all --clear -g ${DSK}
 	sleep 1
 
@@ -30,7 +31,7 @@ if whiptail --defaultno --yesno "COMPLETELY WIPE ${DSK}?" 8 40 ; then
 	sudo parted -s ${DSK} mklabel msdos
 	sudo parted -s ${DSK} -a optimal unit MB -- mkpart primary 1 -1
 
-	case $(whiptail --menu "Choose a filesystem" 17 30 10 \
+	case $(whiptail --menu "Choose a filesystem" 18 30 10 \
 		"1" "udf" \
 		"2" "ext4" \
 		"3" "vfat" \
@@ -38,9 +39,9 @@ if whiptail --defaultno --yesno "COMPLETELY WIPE ${DSK}?" 8 40 ; then
 		3>&1 1>&2 2>&3) in
 			1)
 				command -v mkudffs >/dev/null 2>&1 || { echo "mkudffs required" >&2 ; exit 1 ; }
-				# TODO : check udf module loaded
+				[ $(lsmod | grep -o ^udf) ] || { echo "udf kernel module not loaded" >&2 ; exit 1 ; }
 
-				sudo mkudffs -b 512 --vid=$drivelabel --media-type=hd ${DSK}1
+				sudo mkudffs -b 512 --utf8 --vid=$drivelabel --media-type=hd ${DSK}1
 			;;
 			2)
 				sudo mkfs.ext4 -L "${drivelabel}" ${DSK}1
@@ -53,12 +54,16 @@ if whiptail --defaultno --yesno "COMPLETELY WIPE ${DSK}?" 8 40 ; then
 			;;
 	esac
 
+	sleep 1
+	sudo partprobe ${DSK}
+	sleep 1
 	partboot="/dev/disk/by-label/$drivelabel"
 
 	tmpdir=/tmp/$drivelabel
 	sudo mkdir -p $tmpdir
 
 	sudo mount $partboot $tmpdir
+	trap $(sudo umount $tmpdir) EXIT
 
 	if ( grep -q ${DSK} /etc/mtab ); then
 		echo "info: $partboot mounted at $tmpdir"
@@ -68,12 +73,19 @@ if whiptail --defaultno --yesno "COMPLETELY WIPE ${DSK}?" 8 40 ; then
 
 		sudo chown -R `whoami` $tmpdir
 
-		cp /usr/lib/syslinux/bios/memdisk $tmpdir/boot/grub/
-
 		pushd $tmpdir
-			# TODO : offer git checkout or tar extract
-			curl -L https://github.com/Thermionix/multipass-usb/tarball/master | tar zx --strip 1
+			cp /usr/lib/syslinux/bios/memdisk ./boot/grub/
 			echo "configfile /resources/grub_sources/grub.head.cfg" > ./boot/grub/grub.cfg
+			mkdir -p ./bootisos/
+
+			if whiptail --defaultno --yesno "git checkout repo (able to pull updates), or just extract repository" 8 40 ; then
+				git init
+				git remote add origin https://github.com/Thermionix/multipass-usb.git
+				git fetch
+				git checkout -t origin/master
+			else
+				curl -L https://github.com/Thermionix/multipass-usb/tarball/master | tar zx --strip 1
+			fi
 		popd
 
 		echo "## will unmount $partboot when ready"
